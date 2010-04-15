0b6f6d5b60e1bcacd1d585dce405f61c4679dada
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2010-04-15 16:01:36 +1200

    Initial work on plugin for WordPress.

---
+++ README.txt
@@ -0,0 +1,10 @@
+*** jQuery.Syntax WordPress Plugin ***
+
+jQuery.Syntax is a light-weight client-side syntax highlighter, which dynamically loads external dependencies (js & css) when required. It uses jQuery to make it cross-browser compatible and to simplify integration.
+
+This is the WordPress Plugin. It includes jQuery-1.4.1.min.js and jQuery.Syntax. It works using the standard <pre class="syntax brush-LANGUAGE"> syntax.
+
+For download, documentation, compatibility, please see :
+	<http://www.oriontransfer.co.nz/software/jquery-syntax/>.
+
+For licensing details, please see the included LICENSE.txt.
---
+++ jquery-syntax/jquery-syntax.php
@@ -0,0 +1,47 @@
+<?php
+/*
+Plugin Name: jQuery.Syntax
+Plugin URI: http://www.oriontransfer.co.nz/software/jquery-syntax
+Description: Syntax highlighting using <a href="http://www.oriontransfer.co.nz/software/jquery-syntax">jQuery.Syntax</a> supporting a wide range of popular languages.  Wrap code blocks with <code>&lt;pre class="syntax brush-LANGUAGE"&gt;</code> and <code>&lt;/pre&gt;</code> where <code>LANGUAGE</code> is a jQuery.Syntax supported brush.
+Author: Samuel Williams
+Author URI: http://www.oriontransfer.co.nz/
+Version: 1.5
+*/
+
+# 
+#	This file is part of the "jQuery.Syntax" project, and is licensed under the GNU AGPLv3.
+#
+#	See <jquery.syntax.js> for licensing details.
+#
+#	Copyright 2010 Samuel Williams. All rights reserved.
+#
+
+if (!defined("WP_CONTENT_URL")) define("WP_CONTENT_URL", get_option("siteurl") . "/wp-content");
+if (!defined("WP_PLUGIN_URL"))  define("WP_PLUGIN_URL",  WP_CONTENT_URL        . "/plugins");
+
+function jq_syntax_loaded() {
+	$path = WP_PLUGIN_URL . '/jquery-syntax/';
+	
+	wp_deregister_script('jquery');
+	wp_register_script('jquery', $path . 'jquery-1.4.1.min.js');
+	
+	wp_enqueue_script('jquery');
+	wp_enqueue_script('jquery.syntax', $path . 'jquery-syntax/jquery.syntax.js');
+	wp_enqueue_script('jquery.syntax.cache', $path . 'jquery-syntax/jquery.syntax.cache.js');
+}
+
+function jq_syntax_header () {
+	$plugin_root = plugins_url("/jquery-syntax/");
+	$syntax_root = $plugin_root.'/jquery-syntax/';
+	
+	?>
+	<script type="text/javascript">
+	jQuery.noConflict(); jQuery(document).ready(function($) { Syntax.root = "<?php echo $syntax_root ?>"; $.syntax({layout: 'table', replace: true}) });
+	</script>
+	<?php
+}
+
+add_action('plugins_loaded', 'jq_syntax_loaded');
+add_action('wp_head','jq_syntax_header');
+
+?>
