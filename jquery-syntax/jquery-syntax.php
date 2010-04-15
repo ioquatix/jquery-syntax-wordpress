e309dd10df160cc62c9dbd6cf5cd2b297fc9fe73
Samuel Williams <samuel.williams@oriontransfer.co.nz>
2010-04-15 21:47:50 +1200

    Added escaped support for code in WordPress.

--- jquery-syntax/jquery-syntax.php
+++ jquery-syntax/jquery-syntax.php
@@ -5,7 +5,7 @@ Plugin URI: http://www.oriontransfer.co.nz/software/jquery-syntax
 Description: Syntax highlighting using <a href="http://www.oriontransfer.co.nz/software/jquery-syntax">jQuery.Syntax</a> supporting a wide range of popular languages.  Wrap code blocks with <code>&lt;pre class="syntax brush-LANGUAGE"&gt;</code> and <code>&lt;/pre&gt;</code> where <code>LANGUAGE</code> is a jQuery.Syntax supported brush.
 Author: Samuel Williams
 Author URI: http://www.oriontransfer.co.nz/
-Version: 1.5
+Version: 1.5.1
 */
 
 # 
@@ -18,6 +18,24 @@ Version: 1.5
 
 if (!defined("WP_CONTENT_URL")) define("WP_CONTENT_URL", get_option("siteurl") . "/wp-content");
 if (!defined("WP_PLUGIN_URL"))  define("WP_PLUGIN_URL",  WP_CONTENT_URL        . "/plugins");
+
+function jq_syntax_htmlentities ($match) {
+	$attrs = $match[1];
+	
+	if (preg_match("/escaped/", $attrs)) {
+		$code = $match[2];
+	} else {
+		$code = htmlentities($match[2]);
+	}
+	
+	return "<pre$attrs>$code</pre>";
+}
+
+function jq_syntax_quote($content) {
+	$result = preg_replace_callback('/<pre(.*?)>(.*?)<\/pre>/imsu',jq_syntax_htmlentities, $content);
+	
+	return $result;
+}
 
 function jq_syntax_loaded() {
 	$path = WP_PLUGIN_URL . '/jquery-syntax/';
@@ -35,13 +53,17 @@ function jq_syntax_header () {
 	$syntax_root = $plugin_root.'/jquery-syntax/';
 	
 	?>
-	<script type="text/javascript">
-	jQuery.noConflict(); jQuery(document).ready(function($) { Syntax.root = "<?php echo $syntax_root ?>"; $.syntax({layout: 'table', replace: true}) });
-	</script>
+		<script type="text/javascript">
+			jQuery.noConflict(); jQuery(document).ready(function($) { Syntax.root = "<?php echo $syntax_root ?>"; $.syntax({layout: 'table', replace: true}) });
+		</script>
 	<?php
 }
 
 add_action('plugins_loaded', 'jq_syntax_loaded');
 add_action('wp_head','jq_syntax_header');
+
+add_filter('the_content', 'jq_syntax_quote', 0);
+add_filter('the_excerpt', 'jq_syntax_quote', 0);
+add_filter('comment_text', 'jq_syntax_quote', 0);
 
 ?>
